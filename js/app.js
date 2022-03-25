df760cdca1229b389fa4f7ece137f7cb2a8abaf6
Christ-Amour KAKPO <81099224+iamkepo@users.noreply.github.com>
2022-03-25 13:49:56 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const geometry = new THREE.SphereGeometry( 50, 32, 32 );
-const texture = new THREE.TextureLoader().load('img/s31urju-a-rocky-shore-next-to-a-body-of-water.jpeg');
+const texture = new THREE.TextureLoader().load('./img/s31urju-a-rocky-shore-next-to-a-body-of-water.jpeg');
 const material = new THREE.MeshBasicMaterial( {
   map: texture,
   side: THREE.DoubleSide
